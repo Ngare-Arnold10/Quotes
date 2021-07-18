5563095d0b4e064f879c3bba053fff39d9e224be
reagan2020 <reaganteflon@.com>
2021-07-18 19:39:23 +0300

    update changes

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+// import { Output } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+// import * as EventEmitter from 'events';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-detail',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailComponent implements OnInit {
 
+  @Input()
+  quote!: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  upvote(){
+    this.quote.upvote=this.quote.upvote+1;
+  }
+  downvote(){
+    this.quote.downvote=this.quote.downvote+1;
+  }
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
